7395d54f3b367ab4c60b45d3aa8954f8841f589b
darangshui <ohjamdol@gmail.com>
2023-02-18 23:12:35 +0900

    230218 simpleDiary

--- simplediary/src/DiaryItem.js
+++ simplediary/src/DiaryItem.js
@@ -1,11 +1,11 @@
-const DiaryItem = ({author, content, create_date, emotion, id}) => {
+const DiaryItem = ({ author, content, create_date, emotion }) => {
   return (
     <div className="DiaryItem">
       <div className="info">
         <span>작성자 : {author} | 감정점수 : {emotion}</span>
         <br />
         <span className="date">
-          {new Date(create_date).toLocaleString()}
+          { new Date(create_date).toLocaleString() }
         </span>
       </div>
       <div className="content">
